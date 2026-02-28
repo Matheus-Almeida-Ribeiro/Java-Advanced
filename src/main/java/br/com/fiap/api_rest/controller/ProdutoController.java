dc321dd94ace3293ce2dd513be4157d4cbcdc384
Matheus-Almeida-Ribeiro <matheus.a.ribeiro0505@gmail.com>
2026-02-27 22:42:28 -0300

    Aula- Implementação do DTO com os metodos basicos de validação

--- src/main/java/br/com/fiap/api_rest/controller/ProdutoController.java
+++ src/main/java/br/com/fiap/api_rest/controller/ProdutoController.java
@@ -1,6 +1,7 @@
 package br.com.fiap.api_rest.controller;
 
 import br.com.fiap.api_rest.dto.ProdutoRequest;
+import br.com.fiap.api_rest.dto.ProdutoResponse;
 import br.com.fiap.api_rest.model.Produto;
 import br.com.fiap.api_rest.service.ProdutoService;
 import jakarta.validation.Valid;
@@ -15,9 +16,11 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/produtos")
 public class ProdutoController {
+    private final ProdutoService produtoService;
+    public ProdutoController(ProdutoService produtoService) {
+        this.produtoService = produtoService;
+    }
 
-    @Autowired
-    private ProdutoService produtoService;
 
     //CRUD - Create, Read, Update, Delete
     //POST, GET, PUT, DELETE
@@ -29,12 +32,12 @@ public class ProdutoController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Produto> readProduto(@PathVariable UUID id) {
+    public ResponseEntity<ProdutoResponse> readProduto(@PathVariable UUID id) {
 
         //@PathVariable -> url/produtos?id=1
         //@ResquestParam -> url/produtos/1
 
-        Produto produto = produtoService.read(id);
+        ProdutoResponse produto = produtoService.read(id);
         if (produto == null) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
@@ -42,8 +45,8 @@ public class ProdutoController {
     }
 
     @GetMapping
-    public ResponseEntity<List<Produto>> readProduto() {
-        List<Produto> produtos = produtoService.read();
+    public ResponseEntity<List<ProdutoResponse>> readProduto() {
+        List<ProdutoResponse> produtos = produtoService.read();
         if (produtos.isEmpty()) {
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         }
@@ -52,7 +55,7 @@ public class ProdutoController {
 
     @PutMapping
     public ResponseEntity<Produto> updateProduto(@RequestBody Produto produto) {
-        Produto produtoExistente = produtoService.read(produto.getId());
+        ProdutoResponse produtoExistente = produtoService.read(produto.getId());
         if (produtoExistente == null) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
--- src/main/java/br/com/fiap/api_rest/service/ProdutoService.java
+++ src/main/java/br/com/fiap/api_rest/service/ProdutoService.java
@@ -1,6 +1,8 @@
 package br.com.fiap.api_rest.service;
 
 import br.com.fiap.api_rest.dto.ProdutoRequest;
+import br.com.fiap.api_rest.dto.ProdutoResponse;
+import br.com.fiap.api_rest.mapper.ProdutoMapper;
 import br.com.fiap.api_rest.model.Produto;
 import br.com.fiap.api_rest.repository.ProdutoRepository;
 import org.springframework.beans.BeanUtils;
@@ -10,26 +12,43 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class ProdutoService {
-    @Autowired
-    private ProdutoRepository produtoRepository;
+
+    private final ProdutoRepository produtoRepository;
+    private final ProdutoMapper produtoMapper;
+
+    public ProdutoService(ProdutoRepository produtoRepository, ProdutoMapper produtoMapper) {
+        this.produtoRepository = produtoRepository;
+        this.produtoMapper = produtoMapper;
+    }
 
     //CRUD
     public Produto create(ProdutoRequest produtoRequest) {
         Produto produto = new Produto();
         BeanUtils.copyProperties(produtoRequest, produto);
+
         return produtoRepository.save(produto);
     }
 
-    public Produto read(UUID id) {
+    public ProdutoResponse read(UUID id) {
         Optional<Produto> produto = produtoRepository.findById(id);
-        return produto.orElse(null);
+        if (produto.isEmpty()) {
+            return null;
+        }
+        return produtoMapper.produtoToResponse(produto.get());
     }
 
-    public List<Produto> read(){
-        return produtoRepository.findAll();
+    public List<ProdutoResponse> read(){
+        List<Produto> produtos = produtoRepository.findAll();
+        return produtos
+                .stream()
+                .map( produtoMapper::produtoToResponse)
+                .collect(Collectors.toList());
+
+
     }
 
     public Produto update(Produto produto) {
