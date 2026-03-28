fc9afd4aeaeb8251f44f4620fecfd982db636cca
Matheus-Almeida-Ribeiro <matheus.a.ribeiro0505@gmail.com>
2026-03-27 22:52:17 -0300

    Aula- Arruma endpoints adicionando titulo, versao, descrição, licença e contato.

--- src/main/java/br/com/fiap/api_rest/controller/ProdutoController.java
+++ src/main/java/br/com/fiap/api_rest/controller/ProdutoController.java
@@ -5,6 +5,12 @@ import br.com.fiap.api_rest.dto.ProdutoRequest;
 import br.com.fiap.api_rest.dto.ProdutoResponse;
 import br.com.fiap.api_rest.model.Produto;
 import br.com.fiap.api_rest.service.ProdutoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -20,6 +26,7 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/produtos")
+@Tag(name = "api-produtos")
 public class ProdutoController {
     private final ProdutoService produtoService;
     public ProdutoController(ProdutoService produtoService) {
@@ -29,13 +36,14 @@ public class ProdutoController {
 
     //CRUD - Create, Read, Update, Delete
     //POST, GET, PUT, DELETE
-
+    @Operation(summary = "Cria um novo produto")
     @PostMapping
     public ResponseEntity<Produto> createProduto(@Valid @RequestBody ProdutoRequest produto) {
          Produto produtoSalvo = produtoService.create(produto);
          return new ResponseEntity<>(produtoSalvo, HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Busca um produto por id")
     @GetMapping("/{id}")
     public ResponseEntity<ProdutoResponse> readProduto(@PathVariable UUID id) {
 
@@ -49,6 +57,15 @@ public class ProdutoController {
         return new ResponseEntity<>(produto, HttpStatus.OK);
     }
 
+    @Operation(summary = "Busca produtos por página")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Página de produtos retornado com sucesso!",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ProdutoController.class))),
+            @ApiResponse(responseCode = "404",
+                        description = "Nenhum produto encontrado.",
+                        content = @Content(schema = @Schema))
+    })
     @GetMapping
     public ResponseEntity<Page<ProdutoLista>> readProduto(@RequestParam(defaultValue = "0") Integer pageNumber) {
         Pageable pageable = PageRequest.of(pageNumber, 2, Sort.by("nome").ascending());
@@ -58,7 +75,7 @@ public class ProdutoController {
         }
         return new ResponseEntity<>(produtos, HttpStatus.OK);
     }
-
+    @Operation(summary = "Atualiza o produto específico")
     @PutMapping
     public ResponseEntity<Produto> updateProduto(@RequestBody Produto produto) {
         ProdutoResponse produtoExistente = produtoService.read(produto.getId());
@@ -68,7 +85,7 @@ public class ProdutoController {
         Produto produtoAtualizado = produtoService.update(produto);
         return new ResponseEntity<>(produtoAtualizado, HttpStatus.CREATED);
     }
-
+    @Operation(summary = "Exclui um produto por id")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteProduto(@PathVariable UUID id) {
         produtoService.delete(id);
