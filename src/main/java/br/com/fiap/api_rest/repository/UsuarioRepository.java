5fd0b566cbf6499a4355ce7a1d8f78feae47eb04
Matheus-Almeida-Ribeiro <matheus.a.0505@gmail.com>
2026-05-06 23:13:57 -0300

    feita correção em UsuarioRepository

--- src/main/java/br/com/fiap/api_rest/repository/UsuarioRepository.java
+++ src/main/java/br/com/fiap/api_rest/repository/UsuarioRepository.java
@@ -12,16 +12,3 @@ public interface UsuarioRepository extends Repository<Usuario, UUID> {
     void save(Usuario novoUsuario);
 }
 
-
-package br.com.fiap.api_rest.controller;
-
-import br.com.fiap.api_rest.model.Usuario;
-import org.springframework.data.repository.Repository;
-
-import java.util.UUID;
-
-interface UsuarioRepository extends Repository<Usuario, UUID> {
-    boolean findByLogin(String attr0);
-
-    void save(Usuario novoUsuario);
-}
